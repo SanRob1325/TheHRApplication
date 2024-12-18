f5a894f93853f10e40233ebb3420b52d49eacf69
seanr <seanrobertblitzntonya@gmail.com>
2024-12-18 22:05:19 +0000

    changed to ec2 endpoint

--- hr-frontend/src/api.js
+++ hr-frontend/src/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 //Main API gateway for the backend CRUD functionalities
-const API = axios.create({baseURL: 'http://localhost:3000',headers:{'Content-Type': 'application/json'}});
+const API = axios.create({baseURL: 'http://54.161.211.89:3000',headers:{'Content-Type': 'application/json'}});
 
 export default API
